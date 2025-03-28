f2744b803490002c934e53ed5ee8bef626393f66
sabrinaets <sabrina.guillemette.1@ens.etsmtl.ca>
2025-03-28 18:23:37 -0400

    page inscription et accueil

--- app/src/main/java/com/sg/voyagestp/MainActivity.java
+++ app/src/main/java/com/sg/voyagestp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sg.voyagestp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -55,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         btnInscrire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //lancer l'activité vers l'inscription
+                Intent intent = new Intent(MainActivity.this,Inscription.class);
+                startActivity(intent);
             }
         });
     }
